e243ec1f34e9bd184e4ce85cc34b6678cb36db08
Shreyas Salunkhe <shreyassps29@gmail.com>
2021-09-07 14:21:20 +0530

    Add files via upload

---
+++ build/precache-manifest.6283b475e20f11875ece198c60ef6ec9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803c3d2ff7dd58d5a89f46b16a9784cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22d61bebc8fce6c8cc04",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "249a0d78538d820aaeb7",
+    "url": "/static/js/2.cac91154.chunk.js"
+  },
+  {
+    "revision": "93ce851949c81c033c28f6758a3c33f3",
+    "url": "/static/js/2.cac91154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d61bebc8fce6c8cc04",
+    "url": "/static/js/main.9f7909bd.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  },
+  {
+    "revision": "1cfa24a5e82c665b700f38d9bdf04195",
+    "url": "/static/media/thumbs_up.1cfa24a5.png"
+  },
+  {
+    "revision": "6db44b04476f1385864179e398b16fa0",
+    "url": "/static/media/victory.6db44b04.png"
+  }
+]);
